b47efbcb52a61c81ca6d3bcd9472890a852752b2
ilyass <79806692+illyasshaddoud@users.noreply.github.com>
2024-03-18 20:42:48 +0000

    init

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { addTodo } from "./features/todo/todoSlice";
+import { useDispatch, useSelector } from "react-redux";
+
+const App = () => {
+  const [input, setInput] = useState("");
+  const todos = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="app">
+      {todos.map((todo) => (
+        <div key={todo.id} className="flex">
+          <p>{todo.content}</p>
+          <p>{todo.done}</p>
+        </div>
+      ))}
+      <div>
+        <input
+          className="border-4 border-black"
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button
+          onClick={() => {
+            dispatch(addTodo(input));
+            setInput("");
+          }}
+        >
+          add todo
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
